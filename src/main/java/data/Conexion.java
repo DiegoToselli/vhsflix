d1e9c459a337ed513c8605bae33cbaeca5b346f1
BaridonFrancisco <baridonfrancisco@gmail.com>
2023-12-04 15:16:36 -0300

    creada la entidad pelicula y algunos metodos CRUD

--- src/main/java/data/Conexion.java
+++ src/main/java/data/Conexion.java
@@ -1,5 +1,59 @@
 package data;
 
-public class Conexion {
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import org.apache.commons.dbcp2.BasicDataSource;
 
+public class Conexion {
+    private static String user="root";
+    private static String pass="admin";
+    private static String url="";
+    
+    public static DataSource getDataSource(){
+        BasicDataSource ds=new BasicDataSource();
+        ds.setUrl(url);
+        ds.setUsername(user);
+        ds.setPassword(pass);
+        ds.setInitialSize(100);
+        return ds;
+    }
+    public static Connection getConexion() throws SQLException{
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+        }catch(ClassNotFoundException e){
+            e.printStackTrace();
+    }
+        return getDataSource().getConnection();
+}
+    public static void close(ResultSet rs){
+        try{
+             rs.close();
+            
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+       
+    }
+       public static void close(Statement st){
+        try{
+             st.close();
+            
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+       
+    }
+          public static void close(Connection c){
+        try{
+             c.close();
+            
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+       
+    }
 }
--- src/main/java/data/PeliculaDAO.java
+++ src/main/java/data/PeliculaDAO.java
@@ -1,5 +1,99 @@
 package data;
 
+import java.sql.Blob;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+import java.sql.Statement;
+import model.Pelicula;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
 public class PeliculaDAO {
+
+    public List<Pelicula> recuperarPeliculas() {
+        String query = "SELECT * FROM Peliculas WHERE activa=1";
+        Connection con = null;
+        Statement stp = null;
+        ResultSet rs = null;
+        Pelicula pelicula;
+        List<Pelicula> listaPeliculas = new ArrayList<>();
+        try {
+            con = Conexion.getConexion();
+            stp = con.prepareStatement(query);
+            rs = stp.executeQuery(query);
+            while (rs.next()) {
+                pelicula = new Pelicula();
+                pelicula.setIdPelicula(rs.getInt("idPelcula"));
+                pelicula.setNombre(rs.getString("nombre"));
+                pelicula.setAnioLanzamiento(rs.getInt("anioLanzamiento"));
+                pelicula.setDuracion(rs.getString("duracion"));
+                pelicula.setGenero(rs.getString("genero"));
+                pelicula.setDirector(rs.getString("director"));
+                pelicula.setReparto(rs.getString("reparto"));
+                pelicula.setDescripcion(rs.getString("descripcion"));
+                pelicula.setUrlThriller(rs.getString("urlThriller"));
+                Blob blob=rs.getBlob("portada");
+                byte [] imagenBytes=blob.getBytes(1, (int)blob.length());
+                pelicula.setImagen(imagenBytes);
+                listaPeliculas.add(pelicula);
+
+            }
+
+        } catch (SQLException e) {
+                e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }finally{
+            Conexion.close(rs);
+            Conexion.close(stp);
+            Conexion.close(con);
+        }
+
+        return listaPeliculas;
+    }
+    
+    
+    public void insertarPelicula(Pelicula pelicula){
+        String query="INSERT INTO peliculas (nombre,anioLanzamiento,duracion,genero,director,reparto,descripcion,urlThriller,portada,activa) VALUES (?, ?, ?, ?, ?, ?, ?,?,?,?)";
+         Connection con = null;
+        PreparedStatement stp = null;
+        
+        try {
+        con = Conexion.getConexion();
+        stp = con.prepareStatement(query);
+        stp.setString(1, pelicula.getNombre());
+        stp.setInt(2, pelicula.getAnioLanzamiento());
+        stp.setString(3, pelicula.getDuracion());
+        stp.setString(4, pelicula.getGenero());
+        stp.setString(5, pelicula.getDirector());
+        stp.setString(6, pelicula.getReparto());
+        stp.setString(7, pelicula.getDescripcion());
+        stp.setString(8, pelicula.getUrlThriller());
+        Blob portadaBlob = con.createBlob();
+        portadaBlob.setBytes(1,pelicula.getImagen());
+        stp.setBlob(9, portadaBlob);
+        stp.setBoolean(10, true);
+        int cantidadRegistro=stp.executeUpdate();
+        if(cantidadRegistro>0){
+            System.out.println("se insertaron "+cantidadRegistro+" registros");
+        }else{
+            System.out.println("No se logro insertar ningun registro");
+                    
+        }
+                
+   
+        } catch (SQLException e) {
+                e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }finally{
+            Conexion.close(stp);
+            Conexion.close(con);
+        }
+        
+
+    }
 
 }
